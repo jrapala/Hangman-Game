02586fd4d0973d477c12a9aeb21c393082ef9208
Juliette Rapala <jrapala@gmail.com>
2017-08-31 15:29:11 -0500

    Adding list of words.

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//Logo on top
+//"Press any key to get started!"
+//"Wins"
+//"Current Word"
+//"_ _ _ _ _ _"
+//"Number of Guesses Remaining"
+//"#"
+//"Letters Already Guessed"
+//"A, E, S"
+//Listen for letters that are typed
+//Guessing Correct --> Display Artist and Song Title --> Change Picture --> Play Song
+
+// List of words
+var words = [
+	"Black Sabbath",
+	"Manowar",
+	"Iron Maiden",
+	"Slayer",
+	"Metallica",
+	"Pantera",
+	"Judas Priest",
+	"Megadeth", 
+	"Death", 
+	"Motorhead", 
+	"Carcass", 
+	"Cannibal Corpse", 
+	"Anthrax", 
+	"Sepultura", 
+	"Dio", 
+	"Mercyful Fate", 
+	"Morbid Angel", 
+	"Meshuggah", 
+	"Opeth", 
+	"Testament", 
+	"At The Gates", 
+	"ACDC", 
+	"Celtic Frost", 
+	"Ozzy Osbourne", 
+	"Napalm Death", 
+	"Lamb of God", 
+	"Death", 
+	"Gojira", 
+	"Tool"]
+
+
+]
