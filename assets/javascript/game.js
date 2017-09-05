7296cc0758efa06a450dc9949a43ddf7080e8761
Juliette Rapala <jrapala@gmail.com>
2017-09-05 14:41:09 -0500

    Adding data for videos. Fixing styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,20 @@
 // To Do:
 // 
 // Add side image
+// Add correct winning artist - song text
 // Generate list of YouTube Videos
 // Add sound effect for correct guess
 // Add sound effect for incorrect guess
+// Stop additional wins after won game
+// Test if videos work
+
 
 
 // Global Variables 
 // =====================================================================================
 
 // List of words
-var words = ["Black Sabbath", "Manowar", "Iron Maiden", "Slayer", "Metallica", "Pantera", "Judas Priest", "Megadeth", "Death", "Motorhead", "Carcass", "Cannibal Corpse", "Anthrax", "Sepultura", "Dio", "Mercyful Fate", "Morbid Angel", "Meshuggah", "Opeth", "Testament", "At The Gates", "ACDC", "Celtic Frost", "Ozzy Osbourne", "Napalm Death", "Lamb of God", "Death", "Gojira", "Tool"];
+var words = ["Black Sabbath", "Manowar", "Iron Maiden", "Slayer", "Metallica", "Pantera", "Judas Priest", "Megadeth", "Death", "Motorhead", "Carcass", "Cannibal Corpse", "Anthrax", "Sepultura", "Dio", "Mercyful Fate", "Morbid Angel", "Meshuggah", "Opeth", "Testament", "At The Gates", "ACDC", "Celtic Frost", "Ozzy Osbourne", "Napalm Death", "Lamb of God", "Gojira", "Tool"];
 var listLength = words.length;   								// Find length of word list
 var randomWord = "";											// Word to guess (can includes spaces)
 var letterList = [];											// Word split into array of characters
@@ -33,7 +37,7 @@ function initializeGame() {
 	// Pick random word
 	var randomNumber = Math.floor(Math.random()* listLength);	// Find random number between 0 and length of word list
 	randomWord = words[randomNumber].toUpperCase();				// Choose word using the random number as an index, make uppercase
-	randomWord = "TOOL"	;										// Debug
+	//randomWord = "TOOL"	;										// Debug
 	console.log("Random word: " + randomWord);					// Debug
 	lettersGuessed = [];										// Reset "Letters Already Guessed:"
 	guessesLeft = 0;											// Reset "Guesses Left:"
@@ -52,7 +56,7 @@ function initializeGame() {
 			lettersRemaining++;
 			console.log(gameboard);
 		} else {
-			gameboard.push(" ");								// Replace space with space instead of underscore
+			gameboard.push("<br>");								// Replace space with space instead of underscore
 		}
 	}
 
@@ -78,13 +82,13 @@ function check(letter) {
 	}	
 }
 
-// Check guessed letter
+// Check if guessed letter in word
 function guess(letter) {
 
 	var letterInWord = null;
 
 
-	for (var j = 0; j<letterList.length; j++) {					// If letter in word..
+	for (var j = 0; j<letterList.length; j++) {				// Is letter in word?
 		if (letterList[j] === letter) {
 			letterInWord = true;
 		}
@@ -101,8 +105,8 @@ function guess(letter) {
 			}
 		}
 	} else {
-		console.log("Letter not in word");
-		guessesLeft--;										// Decrease guesses if letter not in word
+		console.log("Letter not in word");					// If letter not in word..
+		guessesLeft--;										// Lose a guess
 		console.log("Guesses left: " + guessesLeft);
 		document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
@@ -116,14 +120,16 @@ function guess(letter) {
 // Check for win or lose
 function checkWin() {
 	if (lettersRemaining === 0) {								// If no more letters left to guess, you win!
-		var giftTitle = "Tool - Schism";
+		var giftTitle = randomWord + " - " + titles[randomWord];
 		document.getElementById("alert").innerHTML = "You win!<br>" + giftTitle + "<br>Press &lt;space&gt; to start next game";
 		wins++;
 		document.getElementById("wins").innerHTML = wins;
-		document.getElementById("video").innerHTML = videos[randomWord];	// Grab video embed text from videos.js
+		document.getElementById("video").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videos[randomWord] + '?autoplay=1" frameborder="0" allowfullscreen></iframe>';	// Grab video embed text from videos.js
 		document.body.onkeyup = function(e){
     		if(e.keyCode == 32){								// Hit space bar to begin next round
         		initializeGame();
+    		} else {
+    			e.preventDefault();
     		}
 		}
 
@@ -140,10 +146,11 @@ function checkWin() {
 initializeGame();
 
 document.onkeyup = function(event) {	
-		var letterGuessed = String.fromCharCode(event.keyCode).toUpperCase();	// Take letter from keystroke, make uppercase
-		check(letterGuessed);													
-		checkWin();
+	var letterGuessed = String.fromCharCode(event.keyCode).toUpperCase();	// Take letter from keystroke, make uppercase
+	check(letterGuessed);													
+	checkWin();
 	}
+
 
 
 
--- assets/javascript/videos.js
+++ assets/javascript/videos.js
@@ -1,5 +1,62 @@
 var videos = {
-	"TOOL" : '<iframe width="560" height="315" src="https://www.youtube.com/embed/UhjG47gtMCo?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+	"TOOL" : "UhjG47gtMCo", 			// working 2017.09.05
+	"BLACK SABBATH" : "LQUXuQ6Zd9w",	//
+	"MANOWAR" : "Dll329ggLIY",			// working 2017.09.05
+	"IRON MAIDEN" : "2G5rfPISIwo",		//
+	"SLAYER" : "z8ZqFlw6hYg",			//
+	"METALLICA" : "bG5wSvi7euI",		// working 2017.09.05
+	"PANTERA" : "Evc3Xtc84N0",			//
+	"JUDAS PRIEST" : "UJ4Nel-aHZM",		//
+	"MEGADETH" : "9d4ui9q7eDM",			//
+	"DEATH" : "woML9M5UBSc",			// working 2017.09.05
+	"MOTORHEAD" : "pWB5JZRGl0U",		// working 2017.09.05
+	"CARCASS" : "5KKYJD09qCk",			//
+	"CANNIBAL CORPSE" : "gNhN6lT-y5U",	//
+	"ANTHRAX" : "uGHsxMqpL0c",			//
+	"SEPULTURA" : "F_6IjeprfEs",		//
+	"DIO" : "EhGEGIBGL",				//
+	"MERCYFUL FATE" : "HqcOVzx4CqY",	//
+	"MORBID ANGEL" : "M0kjr1xesZ8",		// working 2017.09.05
+	"MESHUGGAH" : "qc98u-eGzlc",		// working 2017.09.05
+	"OPETH" : "2kKNT3PdDkE",			//
+	"TESTAMENT" : "bry_6P_1co4",		// working 2017.09.05
+	"AT THE GATES" : "I6cMVf1cXPU",		// working 2017.09.05
+	"ACDC" : "v2AC41dglnM",				//
+	"CELTIC FROST" : "WvPY5BH38B8",		//
+	"OZZY OSBOURNE" : "LCCiwPEdEpg",	//
+	"NAPALM DEATH" : "SIRUzqHTNh8",		// working 2017.09.05
+	"LAMB OF GOD" : "I6whc5Y4CgA",		// working 2017.09.05
+	"GOJIRA" : "FNdC_3LR2AI"			//
 } 
 
+var titles = {
+	"TOOL" : "Schism",
+	"BLACK SABBATH" : "War Pigs",
+	"MANOWAR" : "Warriors of the World",
+	"IRON MAIDEN" : "The Trooper",
+	"SLAYER" : "Raining Blood",
+	"METALLICA" : "Seek and Destroy",
+	"PANTERA" : "Walk",
+	"JUDAS PRIEST" : "Painkiller",
+	"MEGADETH" : "Holy Wars...The Punishment Due",
+	"DEATH" : "Spirit Crusher",
+	"MOTORHEAD" : "Ace of Spades",
+	"CARCASS" : "Unfit For Human Consumption",
+	"CANNIBAL CORPSE" : "Hammer Smashed Face",
+	"ANTHRAX" : "Madhouse",
+	"SEPULTURA" : "Roots Bloody Roots", 
+	"DIO" : "Holy Diver",
+	"MERCYFUL FATE" : "Come to the Sabbath",
+	"MORBID ANGEL" : "Rapture",
+	"MESHUGGAH" : "Bleed",
+	"OPETH" : "The Grand Conjuration", 
+	"TESTAMENT" : "Testament - Return to Serenity",
+	"AT THE GATES" : "The Book Of Sand (The Abomination)",
+	"ACDC" : "Thunderstruck",
+	"CELTIC FROST" : "Circle of the Tyrants",
+	"OZZY OSBOURNE" : "Dreamer",
+	"NAPALM DEATH" : "When All Is Said And Done",
+	"LAMB OF GOD" : "Redneck",
+	"GOJIRA" : "Stranded"
+} 
 
